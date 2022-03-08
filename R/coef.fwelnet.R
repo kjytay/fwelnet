06fb07fa972a302e5b7a00e76cebcdcab6d210ef
Lukas Burk <lukas@quantenbrot.de>
2022-03-08 13:59:56 +0100

    Adjust coef() method for intercept-less cox

--- R/coef.fwelnet.R
+++ R/coef.fwelnet.R
@@ -1,7 +1,14 @@
 #' @importFrom Matrix Matrix
 #' @export
 coef.fwelnet <- function(object, ...) {
-    out <- Matrix::Matrix(rbind(object$a0, object$beta), sparse = TRUE)
-    row.names(out) <- c("(Intercept)", paste0("V", 1:nrow(object$beta)))
+
+    if (object$family == "cox") {
+      out <- Matrix::Matrix(object$beta, sparse = TRUE)
+      row.names(out) <- paste0("V", 1:nrow(object$beta))
+    } else {
+      out <- Matrix::Matrix(rbind(object$a0, object$beta), sparse = TRUE)
+      row.names(out) <- c("(Intercept)", paste0("V", 1:nrow(object$beta)))
+    }
+
     return(out)
 }
