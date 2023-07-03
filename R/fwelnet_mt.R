94e10c41821eca0640062c5d82c05632886fe45b
Lukas Burk <lukas@quantenbrot.de>
2023-07-03 17:49:48 +0200

    Cooper: Add baseline hazard, coef method, allow standardize param

--- R/fwelnet_mt.R
+++ R/fwelnet_mt.R
@@ -18,6 +18,7 @@
 #' @param stratify_by_status (`FALSE`) If `TRUE`, the internal cross-validation folds are sampled by the `status`
 #'  variable, leading to folds with roughly equal proportions of events as in `data`.  
 #'  This is helpful in cases where many censored observations in folds lead to errors during model fits.
+#' @param standardize Passed to [`cv.fwelnet`] and [`cv.glmnet`] respectively.
 #' @param verbose Display informative message on the state of the mt fit.
 #' @param nfolds (`10`) Passed to [`cv.fwelnet`] and [`cv.glmnet`] respectively.
 #' @param include_mt_beta_history `[FALSE]` If `TRUE`, output `list` includes
@@ -30,14 +31,16 @@
 #' @export
 #' @importFrom survival Surv
 #' @importFrom glmnet cv.glmnet
-#' @return A `list` containing per-cause beta matrices for each iteration step
+#' @return An object of class `cooper` with `cv.fwelnet` objects of the final iteration.
+#' If `include_mt_beta_history = TRUE`, contains per-cause beta matrices for each iteration step.
 #'
 fwelnet_mt_cox <- function(data, 
-                           causes = 1:2, # Unused for now
+                           causes = 1:2, # Mostly unused for now
                            mt_max_iter = 5,
                            z_method = "original",
                            stratify_by_status = FALSE,
                            alpha = 1, # pass to glmnet and fwelnet
+                           standardize = TRUE,
                            verbose = FALSE, t = 1, a = 0.5, 
                            thresh = 1e-3,
                            nfolds = 10,
@@ -47,15 +50,16 @@ fwelnet_mt_cox <- function(data,
   # Convert to data.frame just in case it's a data.table
   data <- as.data.frame(data)
   assert_data_frame(data, any.missing = FALSE)
-  assert_integer(causes, min.len = 2)
+  assert_integer(causes, any.missing = FALSE, min.len = 2)
   assert_int(mt_max_iter, lower = 1)
   assert_choice(z_method, choices = c("original", "aligned"))
-  assert_numeric(alpha, lower = 0, upper = 1, len = 1)
+  assert_numeric(alpha, any.missing = FALSE, lower = 0, upper = 1, len = 1)
   assert_logical(verbose, len = 1)
   # FIXME: Rename t variable to avoid clash with base::t
   assert_numeric(t, any.missing = FALSE, len = 1)
-  assert_numeric(alpha, lower = 0, upper = 1, len = 1)
-  assert_numeric(thresh, lower = 0, len = 1)
+  assert_numeric(alpha, any.missing = FALSE, lower = 0, upper = 1, len = 1)
+  assert_numeric(thresh, any.missing = FALSE, lower = 0, len = 1)
+  assert_int(nfolds, lower = 3)
   assert_logical(include_mt_beta_history, len = 1)
 
   # data prep ---------------------------------------------------------------
@@ -90,15 +94,15 @@ fwelnet_mt_cox <- function(data,
   # Predictor matrix shared for all causes
   X <- data[, !(names(data) %in% c("time", "status"))]
   # Treatment-encode factors, ensure numeric design matrix without intercept
-  #X <- model.matrix(~ . -1, X)
-  # Using formula with -1 drops intercept but then creates two variables for binary factors, not great
   X <- model.matrix(~ ., X)
   # Dropping intercept also drops attributes, not sure of this is a problem
-  X <- X[, -1] 
+  X <- X[, -1, drop = FALSE] 
   
   # time is shared between causes
-  y1 <- survival::Surv(data$time, event = status_c1)
-  y2 <- survival::Surv(data$time, event = status_c2)
+  y_list <- list(
+    survival::Surv(data$time, event = status_c1),
+    survival::Surv(data$time, event = status_c2)
+  )
   
   # Stratify by status, generating per-observation fold IDs and passing those to initial
   # cv.glmnet and cv.fwelnet later on
@@ -117,8 +121,8 @@ fwelnet_mt_cox <- function(data,
 
   # Alg step 1) Initialize b1_0, b2_0 at lambda.min glmnet solution
   # for y_1, y_2 respectively
-  gl1 <- glmnet::cv.glmnet(X, y1, family = "cox", alpha = alpha, foldid = y1foldids)
-  gl2 <- glmnet::cv.glmnet(X, y2, family = "cox", alpha = alpha, foldid = y2foldids)
+  gl1 <- glmnet::cv.glmnet(X, y_list[[1]], family = "cox", alpha = alpha, foldid = y1foldids, standardize = standardize)
+  gl2 <- glmnet::cv.glmnet(X, y_list[[2]], family = "cox", alpha = alpha, foldid = y2foldids, standardize = standardize)
 
   b1_0 <- gl1$glmnet.fit$beta[, which(gl1$lambda == gl1$lambda.min)]
   b2_0 <- gl2$glmnet.fit$beta[, which(gl2$lambda == gl2$lambda.min)]
@@ -135,10 +139,12 @@ fwelnet_mt_cox <- function(data,
   # Alg step 2) init k = 0, while k < mt_max_iter:
   # go from 1 to max_iter+1 so we're doing max_iter steps and 1 can be 0.
   k <- 1
-
+  
   while (k < mt_max_iter + 1) {
 
     if (verbose) message("k = ", k)
+    
+    fw_cv_list <- vector(mode = "list", length = 2)
     
     # Stratify by status, generating per-observation fold IDs and passing those cv.fwelnet fits
     # using different fold assignments for each iteration, maybe should assign folds once initially and reuse here?
@@ -151,8 +157,8 @@ fwelnet_mt_cox <- function(data,
     z2 <- abs(beta1[, k, drop = FALSE])
 
     # Get betas from fwelnet fit, requires finding lambda.min via cv first
-    fw2 <- cv.fwelnet(X, y2, z2, family = "cox", alpha = alpha, t = t, a = a, thresh = thresh, foldid = y2foldids, ...)
-    beta2[, k + 1] <- fw2$glmfit$beta[, which(fw2$lambda == fw2$lambda.min)]
+    fw_cv_list[[2]] <- cv.fwelnet(X, y_list[[2]], z2, family = "cox", alpha = alpha, t = t, a = a, thresh = thresh, foldid = y2foldids, standardize = standardize, ...)
+    beta2[, k + 1] <- fw_cv_list[[2]]$glmfit$beta[, which(fw_cv_list[[2]]$lambda == fw_cv_list[[2]]$lambda.min)]
 
     # Alg step 2b)
     z1 <- switch (z_method,
@@ -161,8 +167,8 @@ fwelnet_mt_cox <- function(data,
       stop("z_method ", z_method, " not known")
     )
 
-    fw1 <- cv.fwelnet(X, y1, z1, family = "cox", alpha = alpha, t = t, a = a, thresh = thresh, foldid = y1foldids, ...)
-    beta1[, k + 1] <- fw1$glmfit$beta[, which(fw1$lambda == fw1$lambda.min)]
+    fw_cv_list[[1]] <- cv.fwelnet(X, y_list[[1]], z1, family = "cox", alpha = alpha, t = t, a = a, thresh = thresh, foldid = y1foldids, standardize = standardize, ...)
+    beta1[, k + 1] <- fw_cv_list[[1]]$glmfit$beta[, which(fw_cv_list[[1]]$lambda == fw_cv_list[[1]]$lambda.min)]
 
     # Check beta differences, break if differences are 0
     beta1_diff <- beta1[, k] - beta1[, k + 1]
@@ -189,17 +195,43 @@ fwelnet_mt_cox <- function(data,
     # Increment k at the very last step
     k <- k + 1
   }
+  
+  # get baseline hazards using x = 0 as reference, possibly bad idea according to ?basehaz
+  newdata_zero <- X[1, ] * 0
+  
+  basehazards <- lapply(causes, \(e) {
+    # Accesses the glmnet fit returned with the fwelnet fit with the cv.fwelnet object (naming is bad here yes)
+    bhraw <- survival::survfit(
+      fw_cv_list[[e]]$glmfit$glmfit, s = fw_cv_list[[e]]$lambda.min, 
+      x = X,
+      y = y_list[[e]],
+      newx = newdata_zero,
+      se.fit = FALSE
+    )
+    
+    data.frame(
+      event = e,
+      time = bhraw$time,
+      n.risk = bhraw$n.risk,
+      surv = bhraw$surv,
+      cumhaz = bhraw$cumhaz,
+      hazard = diff(c(0, bhraw$cumhaz))
+    )
+  })
+  
 
   # Returns betas and anything else that might be interesting
   ret <- list(
     # final fwelnet fit objects for later predictions
-    fwfit1 = fw1,
-    fwfit2 = fw2,
+    fwfit1 = fw_cv_list[[1]],
+    fwfit2 = fw_cv_list[[2]],
     # 0-th step glmnet solutions
     glmfit1 = gl1,
     glmfit2 = gl2,
     # model matrix
-    x = X,
+    x = X, y = y_list,
+    # Baseline hazards
+    basehazards = basehazards,
     # Check mt iterations later to assess reasonable values
     mt_iter = k - 1, # Adjust since k can't start at 0
     mt_max_iter = mt_max_iter,
@@ -212,5 +244,6 @@ fwelnet_mt_cox <- function(data,
     ret$beta2 <- beta2
   }
   
+  class(ret) <- c("cooper")
   ret
 }
