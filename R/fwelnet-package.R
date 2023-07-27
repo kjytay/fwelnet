1d712684d437d8a7e3fc29095b1c496609366530
Lukas Burk <lukas@quantenbrot.de>
2023-07-27 14:20:04 +0200

    Import data.table because why not

--- R/fwelnet-package.R
+++ R/fwelnet-package.R
@@ -4,5 +4,14 @@
 
 ## usethis namespace: start
 #' @import checkmate
+#' @importFrom data.table :=
+#' @importFrom data.table .BY
+#' @importFrom data.table .EACHI
+#' @importFrom data.table .GRP
+#' @importFrom data.table .I
+#' @importFrom data.table .N
+#' @importFrom data.table .NGRP
+#' @importFrom data.table .SD
+#' @importFrom data.table data.table
 ## usethis namespace: end
 NULL
