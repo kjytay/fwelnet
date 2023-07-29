be606cba34557c0aee23728899ff0fc2897dcf7d
Lukas Burk <lukas@quantenbrot.de>
2023-07-29 20:53:17 +0200

    Imports

--- R/sim-cr.R
+++ R/sim-cr.R
@@ -1,7 +1,15 @@
 # Reduced version of Binder et al (2009), basically just cox exponential model with normal predictors and equal event and censoring props
 # Uses DGP as per Bender et al (2005)
-#' @noRd
+#' Simple competing risk simulation
+#' @param n,p Number of observations and number of features.
+#' @param beta1,beta2 Coefficient vectors of the Cox-exponential model.
+#' @param lambda1,lambda2 Baseline hazard constants of the models for event 1 and 2.
+#' @param lambda_c Baseline hazard constant for the uninformative censoring distribution.
+#' @param r Correlation between features
 #' @export
+#' 
+#' @examples
+#' sim_cr_coxexp()
 sim_cr_coxexp <- function(n = 100, p = 4, beta1 = runif(p, -1, 1), beta2 = beta1, lambda1 = 0.1, lambda2 = lambda1, lambda_c = 0.1, r = 0) {
   
   checkmate::assert_int(n, lower = 10)
@@ -16,12 +24,11 @@ sim_cr_coxexp <- function(n = 100, p = 4, beta1 = runif(p, -1, 1), beta2 = beta1
   if (r == 0) {
     X <- matrix(rnorm(n * p), ncol = p, nrow = n)
   } else {
-    if (!requireNamespace("mvtnorm")) {
+    if (!requireNamespace("mvtnorm", quietly = TRUE)) {
       stop("Package mvtnorm is required for simulating correlated data.")
     }
     X <- mvtnorm::rmvnorm(n = n, sigma = toeplitz(r^(0:(p - 1))))
   }
-  
   
   # Linear predictors
   lp1 <- X %*% beta1
@@ -51,7 +58,7 @@ sim_cr_coxexp <- function(n = 100, p = 4, beta1 = runif(p, -1, 1), beta2 = beta1
   status_c2[which(di == 1)] <- 0
   status_c2[which(di == 2)] <- 1
   
-  list(
+  structure(list(
     data = xdat,
     Xmat = X,
     time = ti,
@@ -60,6 +67,13 @@ sim_cr_coxexp <- function(n = 100, p = 4, beta1 = runif(p, -1, 1), beta2 = beta1
     beta1 = beta1,
     beta2 = beta2,
     lp1 = lp1, 
-    lp2 = lp2
+    lp2 = lp2),
+    class = "sim_cr"
   )
+}
+
+print.sim_cr <- function(x, ...) {
+  cat(sprintf("Simulated dataset with N = %d and p = %d\n", nrow(x$Xmat), ncol(x$Xmat)))
+  cat("beta1:", sprintf("%.3f", x$beta1), "\n")
+  cat("beta2:", sprintf("%.3f", x$beta2))
 }
